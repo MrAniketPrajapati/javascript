9c3bba7cf6eaaab915ffd561a4d1555c36cc8398
MrAniketPrajapati <143324567+MrAniketPrajapati@users.noreply.github.com>
2023-09-11 18:06:13 +0000

    DataType

--- 01_basics/02_DataTypes.js
+++ 01_basics/02_DataTypes.js
@@ -1,3 +1,17 @@
 "use strict"; // treat all js code as newer version
 
-alert (3 + 3) // we are using node js , not browser
+// alert (3 + 3) // we are using node js , not browser
+
+let Name = "Aniket"
+let Age = 22
+let Boolean = false
+//let state;     --------------undefined
+let bigint = 4545157865
+// Symbol = When we want find/talk unique
+//object
+//null =  standalone value
+
+console.log(Name);
+console.log(typeof Name)                             // String
+console.log(typeof undefined);                       //undefinde
+console.log(typeof null);                            // object 
