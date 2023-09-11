cc6a122d6b1c6f3ae1d64199c934f21184210575
MrAniketPrajapati <143324567+MrAniketPrajapati@users.noreply.github.com>
2023-09-11 23:50:09 +0000

    concertions and operations

---
+++ 01_basics/converstionsOperations.js
@@ -0,0 +1,43 @@
+
+//--------------------Convertions-----------------------
+let score  = null
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valuesInNumber = Number(score)
+//console.log(typeof valuesInNumber);
+//console.log(valuesInNumber);
+// "33abc" => nan Not a Number
+// " 33" => 33
+//true => 1
+//false = 0
+
+let check = 1
+
+let checked  = Boolean(check)
+//console.log(checked);
+
+let takeNumber =  45
+let stringNumber = String(takeNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//--------------------------Opertions----------------------
+
+let num = 15
+let negnum = -num
+//console.log(negnum);  //////////-15
+
+console.log(45+55); //100
+console.log(2*3);  // 6
+console.log(10%3);   //1
+console.log(10/2);  //5
+console.log(100-50); // 50
+console.log(2**3); //8
+
+let str1 = "Aniket"
+let str2 = " Prajapati"
+let str3 = str1 + str2
+console.log(str3);
+
